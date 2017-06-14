0922fedd42dcd1159575f999d5bd9b05ca7759f3
Damian Bushong <katana@odios.us>
2017-06-14 13:45:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# sb-validatejson - Starbound mod helper
+
+sb-validatejson is a node.js utility designed to assist with quickly validating the syntax of the JSON files that Starbound (and Starbound mods) use.
+This is slightly different due to Starbound's use of comments inside of JSON files; they have to be stripped before typical JSON validation.
+
+## installation
+
+Install the latest version of node.js, then:
+
+    npm install damianb/sb-validatejson
+
+Then, create a small stub file (of your own naming) to call the module.
+
+## usage
+
+Just call the exported function - see the example below.
+
+	let validator = require('sb-validatejson')
+	validator({
+		modDir: 'D:\\code\\starbound\\sbmods\\AsteroidOres\\src',
+	}, function(errors) {
+		if(errors) {
+			errors.forEach(function(err) {
+				console.error(err.stack)
+			})
+			process.exit(1)
+		}
+	})
+
+The only arguments taken by the module's function are as follows:
+
+* **options**: a javascript object with any of the following properties:
+* *options.modDir*: the filesystem location where your mod's files live (basically where the _metadata file is).
+* **callback**: a javascript callback to receive any JSON parsing errors discovered when validating the mod's JSON files.  Will only receive one argument, which is be an array of any errors encountered.
+
+And that's it!  Run the js, and you'll get information on any invalid JSON files in your mod.
+
+## todo
+
+CLI utility for verifying everything in current directory.  Would be massively helpful.
---
+++ index.js
@@ -0,0 +1,101 @@
+//
+// sb-validatejson - Starbound mod helper - JSON file validation
+// ---
+// @copyright (c) 2017 Damian Bushong <katana@odios.us>
+// @license MIT license
+// @url <https://github.com/damianb/>
+// @reddit <https://reddit.com/u/katana__>
+//
+/*jslint node: true, asi: true */
+"use strict"
+
+let r_readdir = require('recursive-readdir')
+let jsonlint = require('jsonlint')
+let stripComments = require('strip-json-comments')
+let path = require('path')
+let fs = require('fs-extra')
+let os = require('os')
+
+module.exports = function(options, callback) {
+	if(!options.modDir) {
+		throw new Error('mod source directory MUST be specified')
+	}
+
+	options.modDir += path.sep
+
+	// these should let us ignore files that can't be JSON-patched.
+	let ignoredFiles = [
+		// .disabled and .objectdisabled exist in the Starbound asset files
+		//   we're ignoring them for now, because we probably shouldn't care about these files
+		'*.disabled', // ignored for now. @todo: reconsider?
+		'*.objectdisabled',  // ignored for now. @todo: reconsider?
+		'*.ase', // no idea why an ASE file is in the Starbound assets...lol Chucklefish.
+		'*.md',
+		'*.png',
+		'*.PNG',
+		'*.wav',
+		'*.ogg',
+		'*.ttf',
+		'*.lua',
+		'*.txt',
+		'*.psd',
+		'*.pdn',
+		'*.broken',
+		'*.db',
+		'_metadata',
+		'.metadata',
+		'.gitignore',
+		'.git',
+		'_previewimage'
+	]
+
+	r_readdir(options.modDir, ignoredFiles, function(err, files) {
+		if(err) throw err
+
+		let errors = []
+		files.forEach(function(filePath) {
+			// sanity check
+			if(!filePath.startsWith(options.modDir)) {
+				return
+			}
+
+			let json = null
+			let originalJson = stripComments(fs.readFileSync(filePath, "utf8"))
+			try {
+				// we have to strip comments to deal with Starbound's JSON spec violation of multiline strings
+				json = originalJson.replace(/\r?\n|\r/g, '')
+				json = jsonlint.parse(json)
+			} catch(err) {
+				try {
+					// rerun it in order to try and get an accurate error message.  yeah it's a terrible solution, but...
+					json = jsonlint.parse(originalJson)
+				} catch(err) {
+					err.message = 'Failed to parse file "' + filePath + '"' + os.EOL + err.message
+					errors.push(err)
+					return
+				}
+
+				// uhhh...idk? \_(O_o)_/
+				// we'll swallow the error for now and continue
+				//err.message = 'Failed to parse file "' + filePath + '"' + os.EOL + err.message
+				//errors.push(err)
+				return
+			}
+		})
+
+		if(errors) {
+			if(!!callback) {
+				callback(errors)
+			} else {
+				errors.forEach(function(err) {
+					console.error(err.stack)
+				})
+				process.exit(1)
+			}
+		} else {
+			if(!!callback) {
+				callback()
+			}
+		}
+	})
+}
