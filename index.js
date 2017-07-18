3a07502a1685bff02d9cd796d58cca2e55bde10b
Damian Bushong <katana@odios.us>
2017-07-18 09:44:36 -0500

    Replace the caret completely to prevent batch files from screwing up. (sigh)

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ module.exports = function(options, callback) {
 				callback(errors)
 			} else {
 				errors.forEach(function(err) {
-					let stack = err.stack.replace(/Parse error on line 1:\n\n\^(\nExpecting)/i, "Parse error on line 1: $1")
+					let stack = err.stack.replace('^', '!')
 					console.error(stack)
 				})
 				process.exit(1)
